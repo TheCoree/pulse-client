ee8bd1eb6b56ccf8f32d1760447bf8bb22d647e8
corede <coreespace@gmail.com>
2026-03-08 11:21:36 +0300

    fix: open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || "https://pulse-ttm.com"),
   title: "Pulse TTM",
-  description: "Профессиональный инструмент для работы с календарями, обработки заявок на корректировку и эффективного администрирования.",
+  description: "Профессиональный инструмент для работы с календарями, обработки заявок и эффективного администрирования.",
   openGraph: {
     title: "Pulse TTM",
-    description: "Профессиональный инструмент для работы с календарями, обработки заявок на корректировку и эффективного администрирования.",
+    description: "Профессиональный инструмент для работы с календарями, обработки заявок и эффективного администрирования.",
     siteName: "Pulse TTM",
     locale: "ru_RU",
     type: "website",
+    images: [
+      {
+        url: "https://corede.site/images/pulse-ttm-banner-1920x1080.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Pulse TTM",
     description: "Профессиональный инструмент для работы с календарями, обработки заявок на корректировку и эффективного администрирования.",
+    images: ["https://corede.site/images/pulse-ttm-banner-1920x1080.png"],
   },
 };
 
